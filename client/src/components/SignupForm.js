3051f8d08b1258d59edabff0132a2f3e4e1854f5
Jalon Tuttle <jalontuttle@gmail.com>
2021-12-15 22:37:21 -0500

    updating more frontend code

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -3,6 +3,8 @@ import { Form, Button, Alert } from 'react-bootstrap';
 
 import { createUser } from '../utils/API';
 import Auth from '../utils/auth';
+import { useMutation } from '@apollo/client';
+import { ADD_USER } from '../utils/mutations';
 
 const SignupForm = () => {
   // set initial form state
@@ -12,6 +14,8 @@ const SignupForm = () => {
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
 
+  const addUser = useMutation(ADD_USER);
+
   const handleInputChange = (event) => {
     const { name, value } = event.target;
     setUserFormData({ ...userFormData, [name]: value });
@@ -19,6 +23,20 @@ const SignupForm = () => {
 
   const handleFormSubmit = async (event) => {
     event.preventDefault();
+
+    try {
+      // execute addUser mutation and pass in variable data from form
+      const { data } = await addUser({
+        variables: { ...userFormData}
+      });
+
+      Auth.login(data.addUser.token)
+
+    } catch (e) {
+      console.error(e);
+      setShowAlert(true);
+    }
+  
 
     // check if form has everything (as per react-bootstrap docs)
     const form = event.currentTarget;
