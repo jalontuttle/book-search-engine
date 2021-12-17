6f5d5e82064b50f799225e716d4e82ee9b685ca8
Jalon Tuttle <jalontuttle@gmail.com>
2021-12-17 00:50:13 -0500

    adding README and updating signup to work

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -12,7 +12,7 @@ const SignupForm = () => {
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
 
-  const [createUser] = useMutation(ADD_USER);
+  const [addUser] = useMutation(ADD_USER);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -30,11 +30,11 @@ const SignupForm = () => {
     }
 
     try {
-      const { data } = await createUser({
+      const { data } = await addUser({
         variables: {...userFormData}
       });
 
-      Auth.login(data.createUser.token);
+      Auth.login(data.addUser.token);
     } catch (err) {
       //console.error(err);
       setShowAlert(true);
